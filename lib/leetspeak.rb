77c1f4225792e8725bac1c1f592c2c267cffdc49
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-02-22 14:25:30 -0800

    Pass test.
    Co-authored-by: Austin Landis <austinlandiscode@gmail.com>
    Co-authored-by: Zack Rutledge <Thorgrim88@gmail.com>

--- lib/leetspeak.rb
+++ lib/leetspeak.rb
@@ -1,19 +1,33 @@
-string = "weeping"
+string = "sassafrass"
 def leetspeak(string)
   leetspeak_array = []
   letters = string.split("")
+  x = 0
+  # while (x < letters.length)
   letters.each do |letter|
-    if(letter == "e")
-      leetspeak_array.push('3')
-    elsif(letter == "o")
-      leetspeak_array.push('0')
-    elsif(letter == "I")
-      leetspeak_array.push('1')
+    if(x == 0 && letter == "s")
+      leetspeak_array.push(letter)
+      x +=1
     elsif(letter == "s")
       leetspeak_array.push('z')
+      x +=1
+    elsif(letter == "e")
+      leetspeak_array.push('3')
+      x +=1
+    elsif(letter == "o")
+      leetspeak_array.push('0')
+      x +=1
+    elsif(letter == "I")
+      leetspeak_array.push('1')
+      x +=1
     else
       leetspeak_array.push(letter)
+      x +=1
     end
   end
   leetspeak_array.join()
 end
+
+
+# if(letters.at(1) == "s")
+#   leetspeak_array.push("s")
