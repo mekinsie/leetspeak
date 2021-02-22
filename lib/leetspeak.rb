e3f55df3e7d9ae7faa783157b629dd5f0e4071e4
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-02-22 11:44:29 -0800

    Pass second test.
    Co-authored-by: Austin Landis <austinlandiscode@gmail.com>
    Co-authored-by: Zack Rutledge <Thorgrim88@gmail.com>

--- lib/leetspeak.rb
+++ lib/leetspeak.rb
@@ -1,5 +1,13 @@
-string = "happy"
+string = "weeping"
 def leetspeak(string)
-  string
+  leetspeak_array = []
+  letters = string.split("")
+  letters.each do |letter|
+    if(letter == "e")
+      leetspeak_array.push('3')
+    else
+      leetspeak_array.push(letter)  
+    end
+  end
+  leetspeak_array.join()
 end
-
