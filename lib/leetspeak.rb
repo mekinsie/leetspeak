8e40e51405e00eaa3e6373d5b89eae586ce58065
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-02-22 15:12:47 -0800

    Refactor and create class for method.
    Co-authored-by: Austin Landis <austinlandiscode@gmail.com>
    Co-authored-by: Zack Rutledge <Thorgrim88@gmail.com>

--- lib/leetspeak.rb
+++ lib/leetspeak.rb
@@ -1,32 +1,32 @@
-string = "sassafrass"
-def leetspeak(string)
-  leetspeak_array = []
-  letters = string.split("")
-  
-  x = 0
-  letters.each do |letter|
-    if(letter == " ")
-      leetspeak_array.push(letter)
-      x = 0
-    elsif(x == 0 && letter == "s")
-      leetspeak_array.push(letter)
-      x +=1
-    elsif(letter == "s")
-      leetspeak_array.push('z')
-      x +=1
-    elsif(letter == "e")
-      leetspeak_array.push('3')
-      x +=1
-    elsif(letter == "o")
-      leetspeak_array.push('0')
-      x +=1
-    elsif(letter == "I")
-      leetspeak_array.push('1')
-      x +=1
-    else
-      leetspeak_array.push(letter)
-      x +=1
+class String
+# string = "sassafrass"
+  def leetspeak
+    leetspeak_array = []
+    x = 0
+    self.each_char do |letter|
+      if(letter == " ")
+        leetspeak_array.push(letter)
+        x = 0
+      elsif(x == 0 && letter == "s")
+        leetspeak_array.push(letter)
+        x +=1
+      elsif(letter == "s")
+        leetspeak_array.push('z')
+        x +=1
+      elsif(letter == "e")
+        leetspeak_array.push('3')
+        x +=1
+      elsif(letter == "o")
+        leetspeak_array.push('0')
+        x +=1
+      elsif(letter == "I")
+        leetspeak_array.push('1')
+        x +=1
+      else
+        leetspeak_array.push(letter)
+        x +=1
+      end
     end
+    leetspeak_array.join()
   end
-  leetspeak_array.join()
 end
--- spec/leetspeak_spec.rb
+++ spec/leetspeak_spec.rb
@@ -1,28 +1,26 @@
 require('rspec')
 require('leetspeak')
 
-describe('#leetspeak') do
+describe('String#leetspeak') do
   it('returns a string as is when no Leetspeak rules apply') do
-    expect(leetspeak("happy")).to(eq("happy"))
+    expect("happy".leetspeak).to(eq("happy"))
   end
-
   it('replaces every "e" in a string with a "3"') do
-    expect(leetspeak("weeping")).to(eq("w33ping"))
+    expect("weeping".leetspeak).to(eq("w33ping"))
   end
-
   it('replaces every "o" in a string with a "0"') do
-    expect(leetspeak("pooping")).to(eq("p00ping"))
+    expect("pooping".leetspeak).to(eq("p00ping"))
   end
   it('replaces every "I" in a string with a "1"') do
-    expect(leetspeak("I like Ike")).to(eq("1 lik3 1k3"))
+    expect("I like Ike".leetspeak).to(eq("1 lik3 1k3"))
   end
   it('replaces every "s" in a string with a "z"') do
-    expect(leetspeak("roses")).to(eq("r0z3z"))
+    expect("roses".leetspeak).to(eq("r0z3z"))
   end
   it('does not replace the first letter when it is an s') do
-    expect(leetspeak("sassafrass")).to(eq("sazzafrazz"))
+    expect("sassafrass".leetspeak).to(eq("sazzafrazz"))
   end
   it('replaces letters correctly in a string of words') do
-    expect(leetspeak("I scream you scream we all scream it is very awkward.")).to(eq("1 scr3am y0u scr3am w3 all scr3am it iz v3ry awkward"))
+    expect("I scream you scream we all scream it is very awkward.".leetspeak).to(eq("1 scr3am y0u scr3am w3 all scr3am it iz v3ry awkward."))
   end
 end
