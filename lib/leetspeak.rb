539052cc0ecb7967f25f3cffb959978b332df340
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-02-22 11:25:54 -0800

    Pass first test.
    Co-authored-by: Austin Landis <austinlandiscode@gmail.com>
    Co-authored-by: Zack Rutledge <Thorgrim88@gmail.com>

---
+++ lib/leetspeak.rb
@@ -0,0 +1,9 @@
+string = "happy"
+def leetspeak(string)
+  string
+end
+
+# string = happy
+# def leetspeak(string)
+#   String.leetspeak(string)
+# end
--- spec/leetspeak_spec.rb
+++ spec/leetspeak_spec.rb
@@ -1,15 +1,15 @@
 require('rspec')
 require('leetspeak')
 
-# describe('#leetspeak') do
-#   it('returns a string as is when no Leetspeak rules apply') do
-#     expect(leetspeak("happy")).to(eq("happy"))
-#   end
-# end
-
-
-describe('String#leetspeak') do
+describe('#leetspeak') do
   it('returns a string as is when no Leetspeak rules apply') do
-    expect("happy".leetspeak).to(eq("happy"))
+    expect(leetspeak("happy")).to(eq("happy"))
   end
 end
+
+
+# describe('String#leetspeak') do
+#   it('returns a string as is when no Leetspeak rules apply') do
+#     expect("happy".leetspeak).to(eq("happy"))
+#   end
+# end
