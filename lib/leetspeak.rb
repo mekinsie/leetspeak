a82044e727dadd79862790c579884991ae3a2666
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-02-22 11:47:50 -0800

    Pass third test.
    Co-authored-by: Austin Landis <austinlandiscode@gmail.com>
    Co-authored-by: Zack Rutledge <Thorgrim88@gmail.com>

--- lib/leetspeak.rb
+++ lib/leetspeak.rb
@@ -5,6 +5,8 @@ def leetspeak(string)
   letters.each do |letter|
     if(letter == "e")
       leetspeak_array.push('3')
+    elsif(letter == "o")
+      leetspeak_array.push('0')
     else
       leetspeak_array.push(letter)  
     end
